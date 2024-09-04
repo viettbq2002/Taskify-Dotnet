8b90bf6e3aa5374899ffaf3b3cdf78bc32227e0c
Quốc Việt <thaibinhquocviet2002@gmail.com>
2024-09-04 13:29:34 +0700

    feat(application): intial base service

---
+++ Taskify.Application/Interfaces/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskify.Application.Interfaces
+{
+    public interface IBaseService<T, TResponseDto,TCreateDto,TUpdateDto,TKey> where T : class
+    {
+        Task<TResponseDto> CreateAsync(TCreateDto request);
+        Task<TResponseDto> UpdateAsync(TUpdateDto request);
+        Task<TResponseDto> DeleteAsync(TKey key);
+
+
+
+    }
+}
